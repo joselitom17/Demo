d16c8dd19cdbf6e2327fd3e0b547538873a87690
Jose <josemanuelmartinez2002@gmail.com>
2024-03-11 12:05:10 +0100

    Index del predictions.csv en False 11/03/2024

--- train_model.py
+++ train_model.py
@@ -4,9 +4,10 @@ from models.naive_model import NaiveModel
 df = pd.read_csv('mnist_784_csv.csv')
 model = NaiveModel()
 model.fit(df)
+model.save('model.pkl')
 new_data = df.sample(n=10)
 new_data.to_csv('new_data.csv', index=False)
-model.save('model.pkl')
+
 
 
 
